18dd5caa1da6e84c894778e44031cc133002a327
Jiaxing Hu <jiaxinghu52@gmail.com>
2021-01-02 19:17:48 +0100

    Added eslint for linting and prettier for formatting.

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 120,
+	trailingComma: 'all',
+	arrowParens: 'always',
+	bracketSpacing: true,
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+};
